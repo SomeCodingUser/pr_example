19adbe02f19143af5362fff0a3b2296ec2e3ad3e
Bauer Johannes <jbauer@din.int>
2020-01-30 09:04:01 +0100

    added string.c

---
+++ string.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int my_strlen(char *s)
+{
+ char *p = s;
+ while (*p)
+ ++p;
+ return (p - s);
+}
+
+void debug()
+{
+    printf("irmi\r\n");
+}
+
+void test()
+{
+    printf("Heading\r\n");
+    printf("Text1\r\n");
+    printf("Text2\r\n");
+    printf("Footer\r\n");
+}
+void print2lines()
+{
+    printf("\r\nline_a");
+    printf("\r\nline_a");
+    printf("\r\nline_a");
+    printf("\r\nline_b\r\n");
+}
+
+int main(void)
+{
+ int i;
+ char *s[] = {
+ "Git tutorials",
+ "Tutorials Point"
+ };
+ for (i = 0; i < 2; ++i)
+ printf("string lenght of %s = %d\n", s[i], my_strlen(s[i]));
+ return 0;
+}
